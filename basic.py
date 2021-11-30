962914baf59d888fce9c9baeebdb3a7346dfbef0
Suman Paik <sumanpaikcs@gmail.com>
2021-11-30 21:56:19 +0530

    All the python topics and stuffs are here

---
+++ basic.py
@@ -0,0 +1,116 @@
+# print("hello")
+# print(4+6)
+# if (age<16):
+# 	print("hello")
+
+
+
+# variables in python
+
+# a=34
+# b= "suman"
+# c=4.5
+# print(a)
+# print(b)
+# print(c)
+
+
+
+
+# Type casting in python
+
+# e="23"
+# e=int(e)
+# it maybe str or float
+# print(e)
+
+
+
+# Strings in python
+
+# name="Suman"
+# print(name)
+# print(name[0])       #index starts from 0-th index in python
+# print(name[1:4])     #1-th index element upto 3-th element 
+# or another method
+# nam = '''This is python basics'''
+# print(nam)
+# for removing the useless spaces we can use strip() function
+# pyth= "       this is me       "
+# print(pyth.strip())  #this is how strip() function works
+# print(len(pyth))
+# var=bbame.lower()
+# var= name.upper()
+# var= name.replace("m", "o")
+# print(var)
+
+# str1 = "This is my pen"
+# str2 = " i like it very much"     #this is how to add two string line
+# print(str1 + str2)
+
+# name1 ="This is a "
+# name2 = "Blue"
+# name3 = "Pen"
+# name4 ="I like this pen"
+# temp = "This is a {} {} {}".format(name2,name3,name4)       This is how format() function works 
+                                                              # or you can use f"  {name1} {name2}
+# print(temp)
+
+
+
+
+
+# List in pyton
+
+# lst = [1, 3, 4, 6, 7, 8]
+# var = type(lst)
+# lst[0] = 44      # change the item of the list
+# var = lst[0]
+# var = lst[1:5]
+# print(lst)
+# lenght = len(lst)   #print the length of the list
+# print(lenght)
+# print(var)
+# Add an element at the end of the list
+# lst.append(333)
+# lst.insert(1,333)
+# lst.pop()    # Remove the last element form the list
+# del lst[1]    # Delete an element from the list
+# del lst      # IT is used for delete the whole list
+# lst.clear()
+# print(lst)                #search google list method
+
+
+
+
+# TUPLE IN PTHON
+
+# a =( "Midnapore", "College", "Autonomous")
+# var = a 
+# var= type(a) # output should be a touple
+# a = list(a)  #make the touple as a list
+#Now you can change the index element whatever you want
+# a[0] = "MidnaporeStation"
+# var = type(a)
+# print(var)
+# print(a)  #now you can see the difference, first element of this element is changed 
+
+
+
+
+
+# SETS IN PYTHON
+
+# s1 = {2,2,2,2,2,2,2,2,22,2,2,2,2,2,2,4,4,5,5,66,7}
+# s1.add(33333)        #add is used for adding one element
+# s1.update([3,54,6,75,657,3,656,7767,987])      #update is used foradding multiple element 
+# print(s1)
+# print(len(s1))
+# s1.remove(54)    # or use .discard ------same use but not showing any error if the target element is not there
+# print(s1)
+
+
+
+
+
+#DICTIONARY IN PYTHON
