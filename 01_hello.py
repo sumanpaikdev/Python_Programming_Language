b6031d8ee8b2fc9d43c1a9ee53b59af0e454a0fa
Suman <sumanpaikcs@gmail.com>
2021-12-24 12:51:38 +0530

    pthon basics

---
+++ 01_hello.py
@@ -0,0 +1,19 @@
+# Author: Suman Paik
+# Licenced to: XYZ company
+
+import os   #Built in module
+print("Hello World")
+
+
+
+# next Modules, Comments and pip
+
+# Built in modules     os, abc module etc.
+# External modules     flask, tensorflow etc.
+
+
+
+# comments----->
+
+# 1) Single line comment     #
+# 2) Multi lien comment ------>  '''     '''
---
+++ 02_variablles_and datatypes.py
@@ -0,0 +1,12 @@
+a = 30
+b = 20.45
+c = 'A'
+d = "Suman Paik"
+print(a)
+print(b)
+print(c)
+print(d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
